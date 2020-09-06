6abafd62b8b8c987ffc191388cec75bf4e9165a4
marsiekiera <hatchet17@gmail.com>
2020-09-06 22:46:29 +0200

    delete comments

--- flask/app/views.py
+++ flask/app/views.py
@@ -9,8 +9,3 @@ def index():
     app_name = os.getenv("YHB")
 
     return render_template("index.html")
-
-    # if app_name:
-    #     return f"Hello from {app_name} running in a Docker container behind Nginx!"
-
-    # return "Hello from Flask"
