f2efb05727a6c8c83f730fbf60f1237dd8537042
marsiekiera <hatchet17@gmail.com>
2020-09-08 01:23:16 +0200

    register route add

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,6 +1,6 @@
 from app import app
 import os
-from flask import render_template
+from flask import render_template, request, redirect
 
 @app.route("/")
 def index():
@@ -9,3 +9,12 @@ def index():
     app_name = os.getenv("YHB")
 
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        #to do
+        return redirect("/")
+    else:
+        return render_template("register.html")
