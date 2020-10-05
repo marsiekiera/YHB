72f3d99e9ca5fb7ec3349e6a0f52f3b84b19fffb
marsiekiera <hatchet17@gmail.com>
2020-10-05 22:40:22 +0200

    Create flash

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,10 +1,10 @@
 from app import app
 import os
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash
 import sqlite3 as sql
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.helpers import has_lower, has_number, has_symbol, has_upper, check_password
+from app.helpers import check_password
 
 # Templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -32,27 +32,35 @@ def register():
             user_name = request.form.get("user_name")
 
             if not user_name:
-                return render_template("error.html", message="Please provide User Name")
+                flash('Please provide User Name', 'error')
+                return redirect("/register")
 
             # Check in database if user already exists
             cur.execute("SELECT * FROM users WHERE user_name = ?", (user_name,))
             if cur.fetchone():
-                return render_template("error.html", message="User already exists")
+                flash('User already exists', 'error')
+                return redirect("/register")
 
             # Check correctness of password
             if not request.form.get("password"):
-                return render_template("error.html", message="Please provide password")
+                flash('Please provide password', 'error')
+                return redirect("/register")
             elif not request.form.get("re_password") or request.form.get("password") != request.form.get("re_password"):
-                return render_template("error.html", message="Please re-type the same password")
-            # elif not has_number(request.form.get("password")) or not has_symbol(request.form.get("password")) or not has_lower(request.form.get("password")) or not has_upper(request.form.get("password")):
-            #     return render_template("error.html", message="Need number")
+                flash('Please re-type the same password', 'error')
+                return redirect("/register")
             elif not check_password(request.form.get("password")):
-                return render_template("error.html", message="Please provide password with at least one uppercase and lowercase letter, a number and a symbol.")
-
+                flash('Please provide password with at least one uppercase and lowercase letter, a number and a symbol.', 'error')
+                return redirect("/register")
+            
+            # Create hash password
             hash_password = generate_password_hash(request.form.get("password"))
-            #cur.execute("INSERT INTO users (user_name, hash) VALUES (?, ?)", (user_name, hash_password))
+
+            # Add user to database
+            cur.execute("INSERT INTO users (user_name, hash) VALUES (?, ?)", (user_name, hash_password))
             con.commit()
+
         con.close()
+        flash('You have sucessfully register', 'info')
         return redirect("/")
     else:
         return render_template("register.html")
