7d59792caf75146393f5a81f2eeae992f84ddaae
marsiekiera <hatchet17@gmail.com>
2020-10-06 22:12:09 +0200

    Login added

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,24 +1,84 @@
 from app import app
 import os
-from flask import render_template, request, redirect, flash
+from tempfile import mkdtemp
+from flask import render_template, request, redirect, flash, session
 import sqlite3 as sql
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from app.helpers import check_password
+from app.helpers import check_password, login_required
 
 # Templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-#conn = sqlite3.connect('sqlite.db', check_same_thread=False)
-#db = conn.cursor()
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+
+
 
 @app.route("/")
+@login_required
 def index():
-
+    print(session.get("user_id"))
     # Use os.getenv("key") to get environment variables
     app_name = os.getenv("YHB")
 
     return render_template("index.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        session.clear()
+
+        with sql.connect("sqlite.db") as con:
+            con.row_factory = sql.Row
+            cur = con.cursor()
+            user_name = request.form.get("user_name")
+
+            if not user_name:
+                flash('Please provide User Name', 'error')
+                return redirect("/login")
+            
+            cur.execute("SELECT * FROM users WHERE user_name = ?", (user_name,))
+            records = cur.fetchall()
+
+            if not records:
+                flash('Incorrect user name', 'error')
+                return redirect("/login")
+
+            for row in records:
+                user_id = row[0]
+                user_name = row[1]
+                hash_password = row[2]
+            
+
+            if not hash_password or not check_password_hash(hash_password, request.form.get("password")):
+                flash('Incorrect password', 'error')
+                return redirect("/login")
+            
+            session["user_id"] = user_id
+            session["user_name"] = user_name
+
+            cur.close()
+
+        flash("You were successfully logged in", 'info')
+        return redirect("/")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -60,7 +120,7 @@ def register():
             con.commit()
 
         con.close()
-        flash('You have sucessfully register', 'info')
-        return redirect("/")
+        flash('You have sucessfully register. You can login now.', 'info')
+        return redirect("/login")
     else:
         return render_template("register.html")
