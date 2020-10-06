c9059874224cd4a0d710743abea9e97de3adfaa0
marsiekiera <hatchet17@gmail.com>
2020-10-06 22:14:16 +0200

    Login required added

--- flask/app/helpers.py
+++ flask/app/helpers.py
@@ -1,3 +1,17 @@
+from functools import wraps
+from flask import g, request, redirect, url_for, session
+
+def login_required(f):
+    """
+    https://flask.palletsprojects.com/en/1.0.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get('user_id') is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 def check_password(input_string):
     if any(char.isdigit() for char in input_string) \
         and any(char.islower() for char in input_string) \
