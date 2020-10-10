dec9114746ed8b8a5864174eb536a02109c78f7a
marsiekiera <hatchet17@gmail.com>
2020-10-10 19:40:00 +0200

    routes account and add_transaction add

--- flask/app/views.py
+++ flask/app/views.py
@@ -4,8 +4,9 @@ from tempfile import mkdtemp
 from flask import render_template, request, redirect, flash, session
 import sqlite3 as sql
 from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
 
-from app.helpers import check_password, login_required
+from app.helpers import check_password, login_required, only_digit
 
 # Templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -197,7 +198,8 @@ def add_account():
                 starting_balance = round(float(request.form.get("starting_balance").replace(',','.')), 2)
 
             # Add new user's account to database
-            cur.execute("INSERT INTO account (account_name, user_id, starting_balance) VALUES (?, ?, ?)", (account_name, user_id, starting_balance))
+            cur.execute("""INSERT INTO account (account_name, user_id, starting_balance) 
+                        VALUES (?, ?, ?)""", (account_name, user_id, starting_balance))
             con.commit()  
         con.close()
 
@@ -207,23 +209,174 @@ def add_account():
         return render_template("add_account.html")
 
 @app.route("/account/<account_name>")
+@login_required
 def account(account_name):
-    print(account_name)
+    user_id = session["user_id"]
+    session["account_name"] = account_name
+    today = datetime.now().strftime('%Y-%m-%d')
+    # user's payee list of dict
+    payee_list_dict = []
+    # user's category list of dict
+    category_list_dict = []
+
+    with sql.connect("sqlite.db") as con:
+        cur = con.cursor()
+        # create session account id
+        cur.execute("""SELECT account_id FROM account WHERE user_id = ? 
+            AND account_name = ?""", (user_id, account_name))
+
+        session["account_id"] = cur.fetchone()[0]
+        print(session["account_id"])
 
 
-    return render_template("account.html", account_name=account_name)
+        # create user's payee list of dict
+        cur.execute("""SELECT * FROM payee WHERE user_id = ? 
+            ORDER BY payee_name""",(user_id,))
+        payee_db = cur.fetchall()
+        if not payee_db:
+            flash("You need to add payee before", 'error')
+            return redirect("/payees")
+        for pay in payee_db:
+            payee_dict = {}
+            payee_dict["payee_id"] = pay[0]
+            payee_dict["payee_name"] = pay[1]
+            payee_dict["user_id"] = pay[2]
+            payee_list_dict.append(payee_dict)
+        # create user's category list of dict
+        cur.execute("""SELECT * FROM category WHERE user_id = ? 
+            ORDER BY category_name""", (user_id,))
+        category_db = cur.fetchall()
+        if not category_db:
+            flash("You need to add category before", 'error')
+            return redirect("/categories")
+        for category in category_db:
+            category_dict = {}
+            category_dict["category_id"] = category[0]
+            category_dict["category_name"] = category[1]
+            category_dict["user_id"] = category[2]
+            category_list_dict.append(category_dict)
+    con.close()
+
+    return render_template("account.html", account_name=account_name, 
+        today=today, payee_list_dict=payee_list_dict, 
+        category_list_dict=category_list_dict)
 
 
-@app.route("/transaction/<transaction_id>")
-def transaction(transaction_id):
-    return redirect("/")
+@app.route("/add_transaction", methods=["POST"])
+@login_required
+def add_transaction():
+
+    user_id = session["user_id"]
+    account_id = session["account_id"]
+    date_form = request.form.get("date")
+
+    # Amount
+    amount_form = request.form.get("amount")
+    trans_type = int(request.form.get("transaction_type"))
+    if not only_digit(amount_form):
+        flash("Amount incorrect", 'error')
+        return redirect(f"/account/{ session['account_name'] }")
+    amount = round(float(amount_form.replace(',','.')), 2) * trans_type
+
+
+    payee_name = request.form.get("payee")
+    category_name = request.form.get("category")
+
+    with sql.connect("sqlite.db") as con:
+        cur = con.cursor()
+
+        cur.execute("""SELECT payee_id FROM payee WHERE user_id = ? 
+            AND payee_name = ?""", (user_id, payee_name))
+        payee_id = cur.fetchone()[0]
+
+        cur.execute("""SELECT category_id FROM category WHERE user_id = ? 
+            AND category_name = ?""", (user_id, category_name))
+        category_id = cur.fetchone()[0]
+
+
+
+
+        # cur.execute("""INSERT INTO transactions 
+        #             (date, payee_id, category_id, amount, user_id, account_id)
+        #             VALUES (?, ?, ?, ?, ?, ?)
+        #             """, (date, payee_id, category_id, amount, user_id, account_id))
+    con.close()
+
+
+
+
+    # TESTING
+    print(f"date_form {date_form}")
+    print(f"payee_name {payee_name}")
+    print(f"amount_form {amount_form}")
+    print(f"transaction type {transaction_type}")
+    print(f"transaction type type {type(transaction_type)}")
+    # amount_form = request.form.get("amount") * transaction_type
+    print(f"amount {amount}")
+    print(f"category name {category_name}")
+    # TESTING
+
+
+
+    # # date
+    
+
+    return redirect(f"/account/{ session['account_name'] }")
+
+
+# @app.route("/transaction/<transaction_id>", method=["POST", "GET"])
+# @login_required
+# def transaction(transaction_id):
+#     if request.method == "POST":
+#         return render_template("transaction.html")
+#     else:
+#         return redirect("/")
 
 
 @app.route("/edit_account", methods=["POST", "GET"])
 @login_required
 def edit_account():
     if request.method == "POST":
+        # to do
         flash('Changes saved', "info")
         return redirect("/")
     else:
         return render_template("edit_account.html")
+
+@app.route("/payees", methods=["POST", "GET"])
+@login_required
+def payees():
+    if request.method == "POST":
+        return redirect("/")
+    # to do
+    else:
+        return redirect("/")
+
+@app.route("/categories", methods=["POST", "GET"])
+@login_required
+def categories():
+    if request.method == "POST":
+        return redirect("/")
+    
+        with sql.connect("sqlite.db") as con:
+            con.row_factory = sql.Row
+            cur = con.cursor()
+
+            cur.execute("""SELECT category_id FROM category 
+                        WHERE category_name = ? AND user_id = ?""", (category, user_id))
+            category_id = cur.fetchone()
+            
+
+            if not category_id:
+                # Add new category to database
+                cur.execute("INSERT INTO category (category_name, user_id) VALUES (?, ?)", (category, user_id))
+                con.commit()
+                cur.execute("""SELECT category_id FROM category 
+                            WHERE category_name = ? AND user_id = ?""", (category, user_id))
+                category_id = cur.fetchone()
+                print(f"category_id = { category_id }")
+        con.close()
+
+
+    else:
+        return redirect("/")
