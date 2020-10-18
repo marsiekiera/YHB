a10f5377d37a340491cfc01667ba259a5534fec7
marsiekiera <hatchet17@gmail.com>
2020-10-18 16:49:58 +0200

    Create database update

--- flask/createdb.sql
+++ flask/createdb.sql
@@ -1,5 +1,17 @@
-CREATE TABLE users (user_id INTEGER PRIMARY KEY, user_name TEXT NOT NULL, hash TEXT NOT NULL);
-CREATE TABLE account (account_id INTEGER PRIMARY KEY, account_name TEXT NOT NULL, user_id INTEGER NOT NULL, starting_balance REAL NOT NULL);
-CREATE TABLE category (category_id INTEGER PRIMARY KEY, category_name TEXT NOT NULL, user_id INTEGER NOT NULL);
-CREATE TABLE payee (payee_id INTEGER PRIMARY KEY, payee_name TEXT NOT NULL, user_id INTEGER NOT NULL);
-CREATE TABLE history (history_id INTEGER PRIMARY KEY, date TEXT NOT NULL, payee_id INTEGER NOT NULL, amount REAL NOT NULL, user_id INTEGER NOT NULL, account_id INTEGER NOT NULL);
+CREATE TABLE users (user_id INTEGER PRIMARY KEY, 
+user_name TEXT NOT NULL, hash TEXT NOT NULL);
+
+CREATE TABLE account (account_id INTEGER PRIMARY KEY, 
+account_name TEXT NOT NULL, user_id INTEGER NOT NULL, 
+starting_balance REAL DEFAULT 0, account_hide INTEGER DEFAULT 0);
+
+CREATE TABLE category (category_id INTEGER PRIMARY KEY, 
+category_name TEXT NOT NULL, user_id INTEGER NOT NULL);
+
+CREATE TABLE payee (payee_id INTEGER PRIMARY KEY, 
+payee_name TEXT NOT NULL, user_id INTEGER NOT NULL);
+
+CREATE TABLE transactions (transaction_id INTEGER PRIMARY KEY, 
+date TEXT NOT NULL, payee_id INTEGER NOT NULL, 
+category_id INTEGER NOT NULL, amount REAL NOT NULL, 
+user_id INTEGER NOT NULL, account_id INTEGER NOT NULL);
