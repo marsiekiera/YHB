4ac8d757e4ed63e50c0d4e6e0cf996bab6d98e88
marsiekiera <hatchet17@gmail.com>
2020-11-08 20:06:12 +0100

    update

--- flask/createdb.sql
+++ flask/createdb.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (user_id INTEGER PRIMARY KEY, 
-user_name TEXT NOT NULL, hash TEXT NOT NULL);
+user_name TEXT NOT NULL UNIQUE, hash TEXT NOT NULL);
 
 CREATE TABLE account (account_id INTEGER PRIMARY KEY, 
 account_name TEXT NOT NULL, user_id INTEGER NOT NULL, 
