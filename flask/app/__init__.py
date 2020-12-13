47025531a4bf7f41983ee5d10fd62a2dd1ceb8dc
marsiekiera <hatchet17@gmail.com>
2020-12-13 11:39:56 +0100

    Clean up unnecessary lines of code

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,13 +1,6 @@
 from flask import Flask
-import sqlite3
 
 app = Flask(__name__)
 app.secret_key = b'"\xe2.\xf9C>Z\'q\xad2U\xa4o,\xb2'
 
-# Templates are auto-reloaded
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
 from app import views
-
-#conn = sqlite3.connect('sqlite.db')
-#db = conn.cursor()
